a23d181f9dc953882a9288e2b09ff0eb9d143a5f
javas <sivajigsivajig703@gmail.com>
2023-10-25 05:37:20 +0000

    started working on nextjs

---
+++ shared_prompts/app/layout.jsx
@@ -0,0 +1,25 @@
+import '@styles/globals.css';
+import React from 'react';
+
+
+export const metadata = {
+    title :"promptopia",
+    description : 'Discover & Share AI prompts'
+}
+const RootLayOut = ({children}) => {
+  return (
+    <html lang='en'>
+        <body>
+           <div className="main">
+              <div className="gradient" />
+           </div>
+            
+            <main className="app">
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayOut
---
+++ shared_prompts/app/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>helllo nextJS</div>
+  )
+}
+
+export default Home
